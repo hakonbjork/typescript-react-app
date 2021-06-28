26614aa0c59122bf22effe876796d06d258c4b13
hakonbjork <hakon1307@gmail.com>
2021-06-28 13:02:48 +0200

    project setup

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>React Typescript App</h1>
+                <p>Dette er en setning</p>
+            </div>
+        );
+    }
+}
+
+export default App;
